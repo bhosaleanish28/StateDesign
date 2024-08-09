46504c4dae42f24ca5de39940cd228f03fbdc0b5
Anish.Bhosale <Anish.Bhosale@DELL1527>
2024-08-09 14:09:12 +0530

    Add project files.

---
+++ StatePatternDesign/Model/IATMState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePatternDesign.Model
+{
+    internal interface IATMState
+    {
+        void InsertDebitCard();
+        void EjectDebitCard();
+        void EnterPin();
+        void WithdrawMoney();
+    }
+}
